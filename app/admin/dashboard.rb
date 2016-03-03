524342f073f991ab898ca71d29beb82c2843e3c5
jmcastel <jmcastellucci@gmail.com>
2016-03-03 12:24:46 +0100

    add style

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -16,12 +16,13 @@ ActiveAdmin.register_page "Dashboard" do
     #   column do
     #     panel "Recent Posts" do
     #       ul do
-    #         Post.recent(5).map do |post|
+    #         Post.all do |post|
     #           li link_to(post.title, admin_post_path(post))
     #         end
     #       end
     #     end
     #   end
+    # end
 
     #   column do
     #     panel "Info" do
